757826b92bb5a30641968d616acc363fa15eb4f5
Huynh Truong <91478975+huynhtruongAI@users.noreply.github.com>
2022-01-06 16:51:33 +0700

    Update exercise2.2.py

--- exercise2.2.py
+++ exercise2.2.py
@@ -8,7 +8,6 @@ running_time_hr = 2 * easy_pace_hr + 3 * tempo_pace_hr
 breakfast_hr = start_time_hr + running_time_hr
 breakfast_min = (breakfast_hr-int(breakfast_hr))*60
 breakfast_sec= (breakfast_min-int(breakfast_min))*60
-
 print ('breakfast_hr', int(breakfast_hr) )
 print ('breakfast_min', int (breakfast_min) )
 print ('breakfast_sec', int (breakfast_sec) )

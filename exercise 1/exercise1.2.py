06ee933e49f16be57c1aae18df2eaaf8c3446702
Huynh Truong <91478975+huynhtruongAI@users.noreply.github.com>
2022-01-07 14:41:29 +0700

    Think-Python

---
+++ exercise 1/exercise1.2.py
@@ -0,0 +1,13 @@
+mins = 42
+secs = 42
+total_secs = mins * 60 + secs
+print("There are", total_secs, "seconds in 42 minutes 42 seconds.")
+mile = 1.61
+total_mile = 10 / mile
+print("there are", total_mile, "in 10 kilometer")
+second_per_mile = total_secs / total_mile
+print ("second per mile", second_per_mile)
+minute_per_mile = second_per_mile / 60
+print ("minute per mile:", minute_per_mile)
+mile_per_hour = 60 / minute_per_mile
+print ("mile per hour", mile_per_hour)
---
+++ exercise 2/exercise2.2.py
@@ -0,0 +1,13 @@
+#Exercise 2.2
+#If I leave my house at 6:52 am and run 1 mile at an easy pace (8:15 per mile), then 3 miles at tempo (7:12 per mile) and 1 mile at easy pace again, what time do I get home for breakfast?
+print ('Exercise 2.2')
+start_time_hr = 6 + 52 / 60.0
+easy_pace_hr = (8 + 15 / 60.0 ) / 60.0
+tempo_pace_hr = (7 + 12 / 60.0) / 60.0
+running_time_hr = 2 * easy_pace_hr + 3 * tempo_pace_hr
+breakfast_hr = start_time_hr + running_time_hr
+breakfast_min = (breakfast_hr-int(breakfast_hr))*60
+breakfast_sec= (breakfast_min-int(breakfast_min))*60
+print ('breakfast_hr', int(breakfast_hr) )
+print ('breakfast_min', int (breakfast_min) )
+print ('breakfast_sec', int (breakfast_sec) )
